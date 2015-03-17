2cc7a448d768353eb251935df43c8ae5e94d5d44
Jeremy Deguignet <deguignet@deguignet.local>
2015-03-17 17:00:43 +0100

    ajouts fontions

--- src/prox.jl
+++ src/prox.jl
@@ -1,1 +1,8 @@
 # proximity operators
+function prox_visi(y_est::Array)
+    return (2.*visi + rho_y.*y_est)./(rho_y + 2)
+end
+
+function prox_u(u::Array,μ::const)
+    return (max(1-μ./abs(u),0).*u)
+end
