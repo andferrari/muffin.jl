d601f2d7df6426382b468d8c514dcbe76d090652
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-03-18 18:52:33 +0100

    essai

--- src/admm.jl
+++ src/admm.jl
@@ -2,8 +2,8 @@ include("init.jl")
 
 nfreq = 10
 mydata = datacube[32:96,32:96,1:nfreq]
-mypsf = psfcube[:,:,1:nfreq]
-mypsfadj = flipdim(flipdim(mypsf,1),2)
+mypsf = float64(psfcube[:,:,1:nfreq])
+mypsfadj = float64(flipdim(flipdim(mypsf,1),2))
 
 # precompute
 
@@ -19,16 +19,32 @@ tol2 = Float64[]
 loop = true
 
 rhop = 1.0
-taup
+nfty = size(fty)[1]
+taup = zeros(Float64,nfty,nfty,nfreq)
 
+p = zeros(Float64,nfty,nfty,nfreq)
+x = zeros(Float64,nfty,nfty,10)
 
 while loop
     tic()
     niter +=1
 
     # update x
-    b = fty + taup + rhop*p
-    
+
+
+    # while vecnorm(x[:,:,j+1] - x[:,:,j], 2) > 1E-8
+    #     j += 1
+    #     alpha[:,:,j] = (r[:,:,j]*r[:,:,j])/((cubefilter(cubefilter(p[:,:,1], mypsf), mypsfadj))*p[:,:,j])
+    #     x[:,:,j+1] = x[:,:,j] + alpha[:,:,j]*p[:,:,j]
+    #     r[:,:,j+1] = r[:,:,j] - alpha[:,:,j]*(cubefilter(cubefilter(p[:,:,1], mypsf), mypsfadj))
+    #     beta[:,:,j] = (r[:,:,j+1]*r[:,:,j+1])/(r[:,:,j]*r[:,:,j])
+    #     p[:,:,j+1] = r[:,:,j+1] + beta[:,:,j]*p[:,:,j]
+    #     println(vecnorm(x[:,:,j+1] - x[:,:,j], 2))
+    # end
+
+
+
+
     println("up_x")
 
 
--- src/prox.jl
+++ src/prox.jl
@@ -6,3 +6,27 @@ end
 function prox_u(u::Array,μ)
     return (max(1-μ./abs(u),0).*u)
 end
+
+
+function
+b = fty + taup + rhop*p
+
+x[:,:,:] = 1
+xm = x                                                        # x0
+r = b - (cubefilter(cubefilter(x, mypsf), mypsfadj))  # r0
+rm = r
+p = r                                                # p0
+
+for z = 1:nfreq
+    while vecnorm(x[:,:,z] - xm[:,:,z], 2) > 1E-8
+
+        alpha = (r[:,:,z],r[:,:,z])/dot((cubefilter(cubefilter(p[:,:,z], mypsf), mypsfadj)),p[:,:,z])
+        x = x + alpha*p
+        r = r - alpha*(cubefilter(cubefilter(p, mypsf), mypsfadj))
+        betaa = dot(r[:,:,z],r[:,:,z])/dot(rm[:,:,z],rm[:,:,z])
+        rm = r
+        p = r + betaa*p
+        println(vecnorm(x[:,:,z] - xm[:,:,z])
+        xm = x
+    end
+end
