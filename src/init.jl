0bf7ca1a1f1d9b20a6c17253fc34896095a339bb
André Ferrari <andferrari@gmail.com>
2015-03-18 08:40:14 +0100

    read fits

--- src/init.jl
+++ src/init.jl
@@ -2,7 +2,32 @@
 
 using FITSIO
 
-# load psf fits
+# write cdf file for paraview visualization
+module Writecdf
+export writecdf
 
-f = FITS("../data/makems_wb_meerkat_m30_4h60s.MS.psf.channel.32ch.fits")
-close(f)
+using NetCDF
+    function writecdf{T<:Float64}(filename::String,psfcube,datacube::Array{T,3})
+        nx, ny, nfreq = size(datacube)
+        nccreate(filename,"psf","x",nx,"y",ny,"frequency",nfreq)
+        ncwrite(psfcube,filename,"psf")
+        ncclose()
+    end
+end
+
+# load psf fits file created by meqtrees
+file = FITS("../data/meerkat_psf_32pix_100ch.fits")
+data = read(file[1])
+close(file)
+psfcube = squeeze(data,3)
+nxpsf, nypsf, nfreq = size(psfcube)
+
+file = FITS("../data/cluster.fits")
+data = read(file[1])
+cluster = squeeze(squeeze(data,4),3)
+nxmod, nymod = size(cluster)
+
+data = Array(Float64,nxmod,nymod,nfreq)
+for k = 1:nfreq
+    data[:,:,k] = conv2(cluster,psfcube[:,:,k])
+end
