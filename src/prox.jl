7e92d7dd6d4264ca268cbcd1e48d2a531ba8fefc
André Ferrari <andferrari@gmail.com>
2015-03-19 16:18:28 +0100

    prob cg

--- src/prox.jl
+++ src/prox.jl
@@ -10,7 +10,7 @@ end
 
 function conjgrad(xw::Array,bw::Array,mypsfw::Array,mypsfadjw::Array,mu::Float64;tol = 1e-6,itermax = 1e3)
 
-    r = bw - atax(xw)
+    r = bw - (imfilter_fft(imfilter_fft(xw, mypsfw), mypsfadjw) + mu*xw)
     rm = r
     p = r                                                 #
     iter = 0
@@ -18,7 +18,7 @@ function conjgrad(xw::Array,bw::Array,mypsfw::Array,mypsfadjw::Array,mu::Float64
 
     while loop
         iter += 1
-        Qp = atax(p)
+        Qp = imfilter_fft(imfilter_fft(p, mypsfw), mypsfadjw) + mu*p
         alpha = sum(r.*r)/sum(Qp.*p)
         xw = xw + alpha*p
         r = r - alpha*Qp
