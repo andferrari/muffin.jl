d68e683beb033a8397eb28425a8cb3efc0c48209
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-03-20 14:13:08 +0100

    test

--- src/admm.jl
+++ src/admm.jl
@@ -1,7 +1,8 @@
 include("init.jl")
 include("prox.jl")
 
-nfreq = 10
+#nfreq = 10
+nfreq = size(psfcube)[3]
 mydata = datacube[32:96,32:96,1:nfreq]
 mypsf = float64(psfcube[:,:,1:nfreq])
 mypsfadj = float64(flipdim(flipdim(mypsf,1),2))
@@ -9,6 +10,7 @@ mypsfadj = float64(flipdim(flipdim(mypsf,1),2))
 # precompute
 
 fty = cubefilter(mydata,mypsfadj)
+nfty = size(fty)[1]
 
 # main admm loop
 
@@ -17,56 +19,86 @@ nbitermax = 20
 
 tol1 = Float64[]
 tol2 = Float64[]
-loop = true
 
-nfty = size(fty)[1]
+loop = true
 
 rhop = 1.0
 muesp = 1.0
 mu = muesp + rhop
+
 taup = zeros(Float64,nfty,nfty,nfreq)
-
-
 p = zeros(Float64,nfty,nfty,nfreq)
 x = zeros(Float64,nfty,nfty,nfreq)
 xmm = zeros(Float64,nfty,nfty,nfreq)
 
+errorrec = zeros(Float64,nfty,nfty,nfreq)
+errorest = zeros(Float64,nfreq)
 
-while loop
-    niter +=1
-    println("ADMM iteration: $niter")
+tic()
+    while loop
+        tic()
+        niter +=1
+        println("")
+        println("ADMM iteration: $niter")
 
-    # update x
-    for z = 1:nfreq
-        b = fty[:,:,z] + taup[:,:,z] + rhop*p[:,:,z]
-        x[:,:,z] = conjgrad(x[:,:,z],b,mypsf[:,:,z],mypsfadj[:,:,z],mu,tol=1e-4,itermax = 1e3)
+#         MAPB =  { ( [fty[:,:,z] + taup[:,:,z] + rhop*p[:,:,z]] )  for z=1:nfreq}
+#         MAP = { ( ([x[:,:,z]],[MAPB[z]],[mypsf[:,:,z]],[mypsfadj[:,:,z]],mu) ) for z=1:nfreq}
+#
+#         bla = pmap(conjgrad, MAP)
+#
+# @sync @parallel for z = 1:nfreq
+#
+#                     x[:,:,z] = bla[z]
+#                 end
+
+
+
+        #update x
+        for z = 1:nfreq
+            b = fty[:,:,z] + taup[:,:,z] + rhop*p[:,:,z]
+            x[:,:,z] = conjgrad(x[:,:,z],b,mypsf[:,:,z],mypsfadj[:,:,z],mu,tol=1e-4,itermax = 1e3)
+        end
+
+        # prox positivity
+        tmp = x-taup/rhop
+        p = max(0,tmp)
+
+        # update of Lagrange multipliers
+        taup = taup + rhop*(p-x)
+
+        # computer residues
+        push!(tol1,vecnorm(x - xmm, 2))
+        push!(tol2,vecnorm(x - p, 2))
+
+        # stopping rule
+        if (niter >= nbitermax) || ((tol1[niter] < 1E-3) && (tol2[niter] < 1E-3))
+            loop = false
+        end
+
+        xmm = x
+
+
+        @printf("| - error ||x - xm||: %02.04e \n", tol1[niter])
+        @printf("| - error ||x - xp||: %02.04e \n", tol2[niter])
+        @printf("time for iteration : %f seconds \n", toq())
+
     end
-
-    # prox positivity
-    tmp = x-taup/rhop
-    p = max(0,tmp)
-
-    # update of Lagrange multipliers
-    taup = taup + rhop*(p-x)
-
-    # computer residues
-    push!(tol1,vecnorm(x - xmm, 2))
-    push!(tol2,vecnorm(x - p, 2))
-
-    # stopping rule
-    if (niter >= nbitermax) || ((tol1[niter] < 1E-3) && (tol2[niter] < 1E-3))
-        loop = false
-    end
-
-    xmm = x
-
-    @printf("| - error ||x - xm||: %02.04e \n", tol1[niter])
-    @printf("| - error ||x - xp||: %02.04e \n", tol2[niter])
-
-end
+println("")
+@printf("time for ADMM : %f seconds \n", toq())
 
 figure(2)
 for z = 1:nfreq
-    subplot(5,2,z)
+    subplot(10,10,z)
     imshow(x[:,:,z])
 end
+
+figure(3)
+for z = 1:nfreq
+    errorrec[:,:,z] = data[32:96,32:96,1,1] - x[:,:,z]
+    errorest[z] =  vecnorm(data[32:96,32:96,1,1] - x[:,:,z])/vecnorm(data[32:96,32:96,1,1])
+    subplot(10,10,z)
+    imshow(errorrec[:,:,z])
+end
+
+figure(4)
+plot(errorest)
--- src/prox.jl
+++ src/prox.jl
@@ -36,3 +36,34 @@ function conjgrad(xw::Array,bw::Array,mypsfw::Array,mypsfadjw::Array,mu::Float64
     end
     return xw
 end
+
+
+#
+# function conjgrad( M ;tol = 1e-6,itermax = 1e3)
+#
+#     r = M[z][2] - (imfilter_fft(imfilter_fft(M[z][1], M[z][3],"circular"), M[z][4],"circular") + M[z][5]*M[z][1])
+#     rm = r
+#     p = r
+#     iter = 0
+#     loop = true
+#
+#     while loop
+#         iter += 1
+#         Qp = imfilter_fft(imfilter_fft(p, M[z][3],"circular"), M[z][4],"circular") + M[z][5]*p
+#
+#         alpha = vecnorm(r)^2/sum(Qp.*p)
+#         M[z][1] = M[z][1] + alpha*p
+#         r = r - alpha*Qp
+#         betaa = (vecnorm(r)/vecnorm(rm))^2
+#         rm = r
+#         p = r + betaa*p
+#         crit = vecnorm(r)
+#         if iter > itermax
+#             error("itermax reached")
+#         end
+#         if crit < tol
+#             loop = false
+#         end
+#     end
+#     return M[z][1]
+# end
