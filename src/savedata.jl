655642c5229ef253b983ad2fd4877324b986a2e6
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-05-05 17:50:10 +0200

    save

--- src/savedata.jl
+++ src/savedata.jl
@@ -2,7 +2,7 @@ using HDF5, JLD
 
 function savedata(psfst, skyst, algost, admmst, toolst)
 
-    save("../data/results/savetest_structure.jld", "psfst",psfst,
+    JLD.save("/home/deguignet/savetest_structure.jld", "psfst",psfst,
                                                     "skyst",skyst,
                                                     "algost",algost,
                                                     "admmst",admmst,
