3acc5b8e8bb9fa3e9dad7853d8228851ed4d0b71
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-05-26 14:47:12 +0200

    alpha

--- src/muffinfunc.jl
+++ src/muffinfunc.jl
@@ -404,12 +404,11 @@ function estime_x_par(x::SharedArray{Float64,3},mypsf::Array{Float64,3},mypsfadj
         toto[:,:,z] = eye(256,256)
         zer[:,:,z] = fft(psfpad[:,:,z])
         psfcbe[:,:,z] = 1./ (abs(zer[:,:,z]).^2+mu*toto[:,:,z])
-    end
-
-
-    for z in 1:nfreq
         x[:,:,z] = real(ifft(fft(wlt_b[:,:,z]).*psfcbe[:,:,z]))
     end
+
+
+
     #
     # for z in 1:nfreq
     #     xtmp = fft(wlt_b[:,:,z])
