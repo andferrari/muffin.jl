e4920972f86075d71f93a4431b998900b0516d54
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-05-26 14:56:08 +0200

    fft

--- src/muffinfunc.jl
+++ src/muffinfunc.jl
@@ -403,28 +403,10 @@ function estime_x_par(x::SharedArray{Float64,3},mypsf::Array{Float64,3},mypsfadj
         psfpad[1:255,1:255,z] = mypsf[:,:,z]
         toto[:,:,z] = eye(256,256)
         zer[:,:,z] = fft(psfpad[:,:,z])
-        psfcbe[:,:,z] = 1./ (abs(zer[:,:,z]).^2+mu*toto[:,:,z])
-        x[:,:,z] = real(ifft(fft(wlt_b[:,:,z]).*fft(psfcbe[:,:,z])))
+        psfcbe[:,:,z] = 1./ fft(abs(zer[:,:,z]).^2+mu*toto[:,:,z])
+        x[:,:,z] = real(ifft(fft(wlt_b[:,:,z]).*psfcbe[:,:,z]))
     end
-
-
-
-    #
-    # for z in 1:nfreq
-    #     xtmp = fft(wlt_b[:,:,z])
-    #     println("toto")
-    #     x[:,:,z] = real(imfilter_fft(xtmp,psfcbe[:,:,z]))
-    #     println("titi")
-    # end
-
-
-    # for z in 1:nfreq
-    #     xtmp = fft(wlt_b[:,:,z])
-    #     atmp = fft(mypsf[:,:,z].^2) + mu*toto
-    #     x[:,:,z] = real(ifft(xtmp./atmp))
-    # end
-        return x
-
+    return x
 end
 
 
