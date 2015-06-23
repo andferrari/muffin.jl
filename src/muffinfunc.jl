47ece7faf69da6810cadbac5b22d2f7b220f589f
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-06-23 09:45:29 +0200

    modif

--- src/muffinfunc.jl
+++ src/muffinfunc.jl
@@ -24,8 +24,8 @@ println("MUFFIN initialisation")
         psf = "data/meerkat_m30_25pix.psf.fits"
         obj = "data/2gauss.fits"
     elseif dataobj == "chiara"
-        psf = "data/psfchiara.fits"
-        obj = "data/skychiara.fits"
+        psf = "/home/deguignet/psfchiara.fits"
+        obj = "/home/deguignet/skychiara.fits"
     elseif isempty(dataobj)
         psf = "data/meerkat_m30_25pix.psf.fits"
         obj = "data/M31.fits"
