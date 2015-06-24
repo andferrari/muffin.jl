ac1f890096de8bf4450295aafac062467af88e15
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-06-24 17:00:46 +0200

    mod

--- src/muffinfunc.jl
+++ src/muffinfunc.jl
@@ -148,7 +148,7 @@ function muffinadmm(psfst, skyst, algost, admmst, toolst)
             # println("calcul wlt","  ",a)
 
             tic()
-            for z in 1:nfreq, b in 1:nspat
+            for z in 1:nfreq
                 admmst.wlt[:,:,z] = myidwt(admmst.wlt[:,:,z], nspat, (admmst.taut)[:,:,z,:], rhot,
                                     (admmst.t)[:,:,z,:], spatialwlt)
             end
