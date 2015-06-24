5602f7d7b6e218cb683dcb78417e6c949548262d
Jeremy Deguignet <deguignet.jeremy@gmail.com>
2015-06-24 10:29:34 +0200

    tronq

--- src/muffinfunc.jl
+++ src/muffinfunc.jl
@@ -179,7 +179,8 @@ function muffinadmm(psfst, skyst, algost, admmst, toolst)
             tic()
             @time tmp = admmst.Hx - admmst.taut/rhot
 
-            @time admmst.t = prox_u(tmp,μt/rhot)
+            # @time admmst.t = prox_u(tmp,μt/rhot)
+            @time admmst.t = max(1 - (μt/rhot)./ abs(tmp), 0).*tmp
 
             ##############################
             ###### prox positivity #######
